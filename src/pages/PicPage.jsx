ccd133ebe0f875494ea620f91cbbc57a830f6228
Zi0LEO <leonardo.npl.02@gmail.com>
2024-10-07 17:14:26 +0200

    implemented smaller devices visualization

--- src/pages/PicPage.jsx
+++ src/pages/PicPage.jsx
@@ -10,8 +10,8 @@ export default function PicPage() {
 	const path = `/pics/img${id}.jpg`;
 
 	return (
-		<div className="flex justify-around m-6 flex-wrap">
-			<div className="flex flex-col w-[70vw]">
+		<div className="flex flex-col justify-around m-5 gap-5 flex-wrap xl:flex-row">
+			<div className="flex flex-col w-[100svw-40px] xl:w-[70vw]">
 				<div className="overflow-hidden relative flex flex-col justify-center h-fit rounded-[40px] bg-[#e2dfd2]">
 					<div
 						className="bg-cover bg-center flex items-center p-5 mb-2 profile-banner relative"
@@ -22,9 +22,9 @@ export default function PicPage() {
 					</div>
 					<img
 						src={path}
-						className="absolute h-[80svh] z-[-1] blur-lg w-full object-cover top-[92px] overflow-hidden"
+						className="absolute max-h-[80svh] z-[-1] blur-lg w-full object-cover top-[92px] overflow-hidden"
 					/>
-					<span className="self-center rounded-[20px] h-fit w-fit overflow-hidden">
+					<span className="self-center justify-self-center rounded-[20px] h-fit w-fit overflow-hidden">
 						<img src={path} className="object-contain w-full max-h-[80svh]" />
 					</span>
           <span className="self-end w-fit mx-10 mt-10">
@@ -35,7 +35,7 @@ export default function PicPage() {
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col w-[25vw] h-[85vh] overflow-hidden rounded-[40px] sticky top-[15svh] bg-[#e2dfd2]">
+			<div className="flex flex-col w-[100svw-40px] h-[85vh] overflow-hidden rounded-[40px] sticky top-[15svh] bg-[#e2dfd2] xl:w-[25svw]">
 				<PicRenderer rows={4} picPerRow={2} />
 			</div>
 		</div>
