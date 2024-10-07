561a0c716c713e518ac334b7bbe93a3c9f2765e1
Zi0LEO <leonardo.npl.02@gmail.com>
2024-10-07 15:04:36 +0200

    added banner for pic pages

--- src/pages/PicPage.jsx
+++ src/pages/PicPage.jsx
@@ -14,7 +14,7 @@ export default function PicPage() {
 			<div className="flex flex-col w-[70vw]">
 				<div className="overflow-hidden relative flex flex-col justify-center h-fit rounded-[40px] bg-[#e2dfd2]">
 					<div
-						className="bg-cover bg-center flex items-center p-5 mb-2"
+						className="bg-cover bg-center flex items-center p-5 mb-2 profile-banner relative"
 						style={{ backgroundImage: `url(${profileBanner})` }}
 					>
 						<img src={profileImage} className="mx-10 rounded-full h-12 w-12" />
@@ -22,7 +22,7 @@ export default function PicPage() {
 					</div>
 					<img
 						src={path}
-						className="absolute h-[80svh] z-[-1] blur-lg w-full object-cover top-[92px]"
+						className="absolute h-[80svh] z-[-1] blur-lg w-full object-cover top-[92px] overflow-hidden"
 					/>
 					<span className="self-center rounded-[20px] h-fit w-fit overflow-hidden">
 						<img src={path} className="object-contain w-full max-h-[80svh]" />
